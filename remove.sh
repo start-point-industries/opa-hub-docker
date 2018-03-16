0572f558677b366da2b177f676a63398610bd7d3
Brandon Belcher <belcherbrandon@gmail.com>
2018-03-16 11:13:11 +0000

    Bash scripts

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Removing containers
+docker rm opa-weblogic
+docker rm opa-mysql
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo Stopping containers
+docker stop opa-weblogic
+docker stop opa-mysql
